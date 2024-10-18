ca9e57cf6ffef1f04a0b4f05da36f0c9fabc255a
Tayyab Khan satti129 <90499823+tayyabksatti@users.noreply.github.com>
2024-10-19 01:46:16 +0500

    code pushed

--- src/sections/appsite/app/donor-dashboard-section/index.tsx
+++ src/sections/appsite/app/donor-dashboard-section/index.tsx
@@ -40,7 +40,7 @@ export function DonorDashboardSection() {
         <Button
           variant="outlined"
           onClick={() => {
-            router.push("add-balance");
+            router.push("/add-balance");
           }}
         >
           Add Balance
@@ -62,7 +62,7 @@ export function DonorDashboardSection() {
             Current Balance
           </Typography>
         </Box>
-        <Button variant="contained">Donate Now</Button>
+        <Button variant="contained" onClick={()=>{router?.push('/donor-dashboard/donate')}}>Donate Now</Button>
       </Stack>
       <Stack direction="row" justifyContent="space-between">
         <Typography variant="h5">Trusted Partner</Typography>
