1b9999c34666225b164bcce4a62226139cde83d9
Tayyab Khan satti129 <90499823+tayyabksatti@users.noreply.github.com>
2024-10-22 19:56:40 +0500

    code push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,34 @@
 "use client";
 
-import SaleFooter from "../layouts/saleSite/footer/sale-footer";
-import SaleHeader from "../layouts/saleSite/header/Sale-header";
-import MainSaleSite from "../sections/salesite";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
+// import SaleFooter from "../layouts/saleSite/footer/sale-footer";
+// import SaleHeader from "../layouts/saleSite/header/Sale-header";
+// import MainSaleSite from "../sections/salesite";
+
+
+
+// export default function Home() {
+
+//   return (
+//     <>
+//        {/* <SaleHeader />
+//       <MainSaleSite />
+//       <SaleFooter/>  */}
+      
+
+//     </>
+//   );
+// }
 
 
 export default function Home() {
+  const router = useRouter();
 
-  return (
-    <>
-       <SaleHeader />
-      <MainSaleSite />
-      <SaleFooter/> 
-      
+  useEffect(() => {
+    router.push('/sign-in');
+  }, [router]);
 
-    </>
-  );
+  return null; // or empty fragment <>
 }
-
-
