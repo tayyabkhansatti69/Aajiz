ba28f90159db577f876bd99031b1371996f5cfad
Tayyab Khan satti129 <90499823+tayyabksatti@users.noreply.github.com>
2024-10-26 03:43:48 +0500

    code pushed

--- src/sections/appsite/app/notification/index.tsx
+++ src/sections/appsite/app/notification/index.tsx
@@ -1,31 +1,39 @@
 
 import { useGetNotificationQuery } from "@/src/services/donor/notification/notification-api";
-import { Box, Button, Grid, Paper, Typography } from "@mui/material";
+import { Box, Button, CircularProgress, Grid, IconButton, Paper, Typography } from "@mui/material";
 import { useState } from "react";
 import relativeTime from 'dayjs/plugin/relativeTime';
 import dayjs from "dayjs";
 import CustomPagination from "@/src/components/custom-pagination";
-
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { useRouter } from "next/navigation";
+import { NoContentFound } from "@/src/components";
 // Extend dayjs with the relativeTime plugin
 dayjs.extend(relativeTime);
 
 function NotificationSection() {
-    
-    
+
+
     const [param, setParams] = useState({
         offset: 0,
         limit: 10
     });
 
-    const { data, } = useGetNotificationQuery({ ...param });
-    console.log(data)
+    const { data, isLoading } = useGetNotificationQuery({ ...param });
+
     const [page, setPage] = useState(data?.current_page);
     const [pageLimit, setPageLimit] = useState(data?.limit)
 
-
+    const router = useRouter()
     return (
         <Grid pt={2} container>
             <Grid xs={12} item>
+                <Box gap={1} display={'flex'} alignItems={'center'}>
+                    <IconButton onClick={() => { router.push('/dashboard') }}> <ArrowBackIcon /></IconButton>
+                    <Typography variant='h6'>Back</Typography>
+                </Box>
+            </Grid>
+            <Grid xs={12} item mt={1}>
                 <Typography variant='h4'>Notification</Typography>
             </Grid>
 
@@ -34,34 +42,44 @@ function NotificationSection() {
 
 
                     <Box px={2}>
-                        {data?.notifications?.map((sectionData, sectionIndex) => (
-                            <Grid item xs={12} key={sectionIndex}>
-
-                                {/* Map through each notification item */}
-
-                                <NotificationItem
-                                    key={sectionData?.id}
-                                    title={sectionData?.notification_message}
-                                    time={sectionData?.notification_date}
-
-                                />
-
-                            </Grid>
+                        {isLoading && (
+                            <Box minHeight={300} display="flex" justifyContent="center" alignItems="center">
+                                <CircularProgress />
+                            </Box>
+                        )}
+                        {!isLoading && (data?.notifications?.length === 0 ? (
+                            <Box minHeight={300} display="flex" justifyContent="center" alignItems="center">
+                                <NoContentFound />
+                            </Box>
+                        ) : (
+                            data?.notifications?.map((sectionData, sectionIndex) => (
+                                <Grid item xs={12} key={sectionIndex}>
+                                    {/* Map through each notification item */}
+                                    <NotificationItem
+                                        key={sectionData?.id}
+                                        title={sectionData?.notification_message}
+                                        time={sectionData?.notification_date}
+                                    />
+                                </Grid>
+                            ))
                         ))}
                     </Box>
+
                     <Box p={2}>
-                    <CustomPagination
-                        count={data?.total_pages}
-                        currentPage={page}
-                        totalRecords={data?.total_pages}
-                        onPageChange={(newPage: any) => { setPage?.(newPage);  setParams((prev) => {
-                            return { ...prev, offset: (newPage - 1) * 10 };
-                        }); }}
-                        setPage={setPage}
-                        pageLimit={pageLimit}
-                        setPageLimit={setPageLimit}
-                    />
-</Box>
+                        <CustomPagination
+                            count={data?.total_pages}
+                            currentPage={page}
+                            totalRecords={data?.total_pages}
+                            onPageChange={(newPage: any) => {
+                                setPage?.(newPage); setParams((prev) => {
+                                    return { ...prev, offset: (newPage - 1) * 10 };
+                                });
+                            }}
+                            setPage={setPage}
+                            pageLimit={pageLimit}
+                            setPageLimit={setPageLimit}
+                        />
+                    </Box>
                 </Paper>
             </Grid>
         </Grid>
