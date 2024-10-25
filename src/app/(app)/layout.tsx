0acb8dc5318756a1a8f05726bc25bf266bf2e2cc
Shahbaz Mughal <shahbazmughal968@gmail.com>
2024-10-26 02:49:53 +0500

    #comments

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -2,10 +2,17 @@
 
 import DashboardLayout from "@/src/layouts/dashboard-layout";
 import React from "react";
+import Container from "@mui/material/Container";
 
 function layout(props: any) {
   const { children } = props;
-  return <DashboardLayout>{children}</DashboardLayout>;
+  const myValue: any = localStorage.getItem("rememberMe");
+  const data: any = JSON.parse(myValue);
+  return data?.Data_User?.kyc_verify ? (
+    <DashboardLayout>{children}</DashboardLayout>
+  ) : (
+    <Container sx={{ m: "auto" }}>{children}</Container>
+  );
 }
 
 export default layout;
--- src/sections/appsite/auth/sign-in-form/useSignInForm.tsx
+++ src/sections/appsite/auth/sign-in-form/useSignInForm.tsx
@@ -1,86 +1,102 @@
-import * as Yup from 'yup';
-import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';  // Import the resolver
-import { setLocalStorage } from '@/src/components/utils';
-import { useRouter } from 'next/navigation';
-import { useLoginMutation } from '@/src/services/auth-api';
+import * as Yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup"; // Import the resolver
+import { setLocalStorage } from "@/src/components/utils";
+import { useRouter } from "next/navigation";
+import { useLoginMutation } from "@/src/services/auth-api";
 // import { removeLocalStorage } from '@/src/utils';
-import toast from 'react-hot-toast';
-import { useState } from 'react';
+import toast from "react-hot-toast";
+import { useState } from "react";
 // import { useState } from 'react';
-
 
 // Yup schema
 export const Schema = Yup.object().shape({
-  username : Yup.string().email('Invalid email address').required('Email is required'),
-  password: Yup.string().min(8, 'Password must be at least 8 characters long').required('Password is required'),
+  username: Yup.string()
+    .email("Invalid email address")
+    .required("Email is required"),
+  password: Yup.string()
+    .min(8, "Password must be at least 8 characters long")
+    .required("Password is required"),
 });
 
 // Default values
 export const defaultValues = {
-  username : "",
+  username: "",
   password: "",
 };
 
 // Custom hook
 export const UseSignInForm = () => {
-  const [loginPost]=useLoginMutation()
+  const [loginPost] = useLoginMutation();
   const [showPassword, setShowPassword] = useState(false);
   const handleClickShowPassword = () => {
     setShowPassword((prev) => !prev);
-};
+  };
 
-const router=useRouter()
-    const methods = useForm<any>({
-    resolver: yupResolver(Schema),  // Pass Yup schema to the resolver
+  const router = useRouter();
+  const methods = useForm<any>({
+    resolver: yupResolver(Schema), // Pass Yup schema to the resolver
     defaultValues: defaultValues,
   });
 
   const { handleSubmit } = methods;
-  
 
   async function onSubmit(data: any): Promise<any> {
-
     const { username, password } = data;
-  
+
     // Save the 'remember me' data to local storage
 
-  
     try {
       const credentials = new URLSearchParams(); // Ensure form data is sent correctly
       credentials.append("username", username);
       credentials.append("password", password);
-  
+
       // Perform login mutation using RTK Query
       const respsone = await loginPost(credentials).unwrap();
-      console.log(respsone,"lklkkk")
-      setLocalStorage('rememberMe', respsone);
-      
-      toast.success(respsone?.message || "Sign in successfully!");
-  
-      // Role-based redirection logic
-      switch (respsone?.Data_User?.account_type) {
-        case "donor":
-          router.push("/dashboard");
-          break;
-        case "partner":
-          router.push("/dashboard");
-          break;
-        case "admin":
-          router.push("/dashboard/admin");
-          break;
-        default:
-          null
-          break;
+      if (
+        respsone?.Data_User?.is_kyc === "applied" &&
+        respsone?.Data_User?.kyc_verify === false
+      ) {
+        toast.error("Your KYC verification status is pending from admin side!");
+      } else if (
+        respsone?.Data_User?.is_kyc === "not_applied" &&
+        respsone?.Data_User?.kyc_verify === false
+      ) {
+        setLocalStorage("rememberMe", respsone);
+
+        toast.success(respsone?.message || "Sign in successfully!");
+        router.push("/dashboard");
+
+        // Role-based redirection logic
+        // switch (respsone?.Data_User?.account_type) {
+        //   case "donor":
+        //     router.push("/dashboard");
+        //     break;
+        //   case "partner":
+        //     router.push("/dashboard");
+        //     break;
+        //   case "admin":
+        //     router.push("/dashboard/admin");
+        //     break;
+        //   default:
+        //     null;
+        //     break;
+        // }
+      } else if (
+        respsone?.Data_User?.is_kyc === "applied" &&
+        respsone?.Data_User?.kyc_verify === true
+      ) {
+        setLocalStorage("rememberMe", respsone);
+
+        toast.success(respsone?.message || "Sign in successfully!");
+        router.push("/dashboard");
       }
     } catch (error: any) {
       console.error(error);
-  
-        toast.error(error?.data?.message || "Something went wrong!");
-      
+
+      toast.error(error?.data?.message || "Something went wrong!");
     }
   }
-  
 
   return {
     methods,
