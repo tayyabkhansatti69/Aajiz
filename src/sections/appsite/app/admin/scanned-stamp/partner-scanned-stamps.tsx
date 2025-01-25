f6dabd4029e8e117bff1092906cf8eeb3cba54f7
Shahbaz Mughal <shahbazmughal968@gmail.com>
2025-01-26 02:40:36 +0500

    #comments

--- src/sections/appsite/app/admin/scanned-stamp/partner-scanned-stamps.tsx
+++ src/sections/appsite/app/admin/scanned-stamp/partner-scanned-stamps.tsx
@@ -1,12 +1,24 @@
+// import { useState } from "react";
 import { CustomTable } from "@/src/components";
-import { useGetRecentScannedStampsListQuery } from "@/src/services/admin/scanned-stamp/scanned-stamp-api";
-import { useState } from "react";
+// import { useGetRecentScannedStampsListQuery } from "@/src/services/admin/scanned-stamp/scanned-stamp-api";
+import { Button } from "@mui/material";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import { useRouter } from "next/navigation";
 
 function PartnerScannedStamps() {
-  const [params, setParams] = useState({ limit: 10, offSet: 0 });
-  const { data, isLoading, isError, isFetching, isSuccess } =
-    useGetRecentScannedStampsListQuery(params);
-
+  const { push } = useRouter();
+  // const [params, setParams] = useState({ limit: 10, offSet: 0 });
+  // const { data, isLoading, isError, isFetching, isSuccess } =
+  //   useGetRecentScannedStampsListQuery(params);
+  const data = [
+    {
+      id: 1,
+      businessName: "KFC",
+      stampType: "Physical",
+      cardNumber: "5121754",
+      price: 500,
+    },
+  ];
   const columns = [
     {
       accessorFn: (row: any) => row.businessName ?? "-",
@@ -37,30 +49,40 @@ function PartnerScannedStamps() {
       isSortable: false,
     },
     {
-      accessorFn: (row: any) => row.price ?? "-",
-      id: "price",
-      cell: (info: any) => `${info.getValue()} RS.`,
+      accessorFn: (row: any) => row.action ?? "-",
+      id: "action",
+      cell: ({ row }: any) => (
+        <Button
+          variant="text"
+          startIcon={<VisibilityIcon />}
+          onClick={() => {
+            push(`view-scanned-stamp?id=${row?.original?.id}`);
+          }}
+        >
+          View
+        </Button>
+      ),
       header: () => <span>Action</span>,
       isSortable: false,
     },
   ];
   return (
     <CustomTable
-      data={data?.body}
+      data={data}
       columns={columns}
-      isLoading={isLoading}
-      isFetching={isFetching}
-      isError={isError}
-      isSuccess={isSuccess}
+      // isLoading={isLoading}
+      // isFetching={isFetching}
+      // isError={isError}
+      isSuccess={true}
       isPagination
       // showSerialNo
-      totalPages={data?.total_pages ?? 1}
-      currentPage={data?.current_page ?? 1}
-      onPageChange={(onPageData: any) => {
-        setParams((prev) => {
-          return { ...prev, offset: (onPageData - 1) * 10 };
-        });
-      }}
+      // totalPages={data?.total_pages ?? 1}
+      // currentPage={data?.current_page ?? 1}
+      // onPageChange={(onPageData: any) => {
+      //   setParams((prev) => {
+      //     return { ...prev, offset: (onPageData - 1) * 10 };
+      //   });
+      // }}
     />
   );
 }
