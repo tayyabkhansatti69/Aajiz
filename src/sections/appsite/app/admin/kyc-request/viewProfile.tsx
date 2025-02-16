6134dc7d69e93c18fdbf79a80e49e708008334f6
Shahbaz Mughal <shahbazmughal968@gmail.com>
2025-02-16 13:59:51 +0500

    #comments

--- src/sections/appsite/app/admin/kyc-request/viewProfile.tsx
+++ src/sections/appsite/app/admin/kyc-request/viewProfile.tsx
@@ -1,19 +1,42 @@
+import { FormProvider, RHFTextField } from "@/src/components/rhf";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import CloseIcon from "@mui/icons-material/Close";
+import DoneIcon from "@mui/icons-material/Done";
+import VerifiedIcon from "@mui/icons-material/Verified";
+import { LoadingButton } from "@mui/lab";
 import {
   Avatar,
   Button,
   Card,
+  Chip,
+  Grid2 as Grid,
   IconButton,
   Stack,
   Typography,
 } from "@mui/material";
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import { useRouter } from "next/navigation";
-import DoneIcon from "@mui/icons-material/Done";
-import CloseIcon from "@mui/icons-material/Close";
+import { UseViewProfile } from "./useViewProfile";
+import { useApproveKYCMutation } from "@/src/services/admin/kyc-requests/kyc-requests-api";
+import toast from "react-hot-toast";
 
 export function ViewProfileSection() {
-  const { back } = useRouter();
+  const { methods, handleSubmit, onSubmit, isLoading, data, back, id } =
+    UseViewProfile();
+  const [approveKyc] = useApproveKYCMutation();
+  const onSubmitKyc = async (approve: any): Promise<void> => {
+    const body = {
+      id,
+      approve,
+    };
 
+    try {
+      const res: any = await approveKyc(body).unwrap();
+      back();
+      toast.success(res?.message ?? `KYC Status Update Successfully!`);
+    } catch (error: any) {
+      const errMsg = error?.data?.message;
+      toast.error(errMsg ?? "Something Went Wrong!");
+    }
+  };
   return (
     <Stack>
       <Button
@@ -27,7 +50,10 @@ export function ViewProfileSection() {
         Back
       </Button>
       <Card sx={{ p: 4 }}>
-        <Stack direction="row" justifyContent="space-between">
+        <Stack
+          direction={{ md: "row", xs: "column" }}
+          justifyContent="space-between"
+        >
           <Stack direction="row" spacing={2}>
             <Avatar sx={{ width: 100, height: 100 }} />
             <Stack spacing={1}>
@@ -43,8 +69,8 @@ export function ViewProfileSection() {
               >
                 Status
               </Typography>
-              <Typography variant="h6">Name</Typography>
-              <Typography>Type</Typography>
+              <Typography variant="h6">{data?.body?.name}</Typography>
+              <Typography>{data?.body?.account_type}</Typography>
             </Stack>
           </Stack>
           <Stack direction="row" spacing={1}>
@@ -54,6 +80,13 @@ export function ViewProfileSection() {
                 backgroundColor: "#0EBDBE",
                 width: "fit-content",
                 height: "fit-content",
+                ":hover": {
+                  color: "white",
+                  backgroundColor: "#0EBDBE",
+                },
+              }}
+              onClick={() => {
+                onSubmitKyc(true);
               }}
             >
               <DoneIcon />
@@ -64,26 +97,171 @@ export function ViewProfileSection() {
                 backgroundColor: "#F36F56",
                 width: "fit-content",
                 height: "fit-content",
+                ":hover": {
+                  color: "white",
+                  backgroundColor: "#F36F56",
+                },
+              }}
+              onClick={() => {
+                onSubmitKyc(false);
               }}
             >
               <CloseIcon />
             </IconButton>
           </Stack>
         </Stack>
-        <Typography sx={{ px: 10 }}>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus
-          quibusdam facere ab ex consectetur repellendus vel maxime voluptate
-          nesciunt. Aspernatur impedit quam officiis? Expedita sint distinctio
-          voluptate assumenda eius dolorem perspiciatis inventore, sapiente
-          optio culpa ratione exercitationem? Nulla laudantium rerum commodi,
-          dolorum ipsum ullam ab nam corrupti. Voluptate consequatur optio,
-          culpa voluptatum assumenda perferendis ullam expedita impedit ratione!
-          Veritatis incidunt numquam, nam quisquam repudiandae corrupti magni?
-          Natus dicta hic corrupti nisi libero beatae, provident voluptatem odit
-          voluptatibus cum. Laboriosam, saepe repellendus! Sequi ipsum eligendi
-          tempora, esse harum aut dolor ea officia dicta porro repellendus
-          reprehenderit explicabo maxime atque veniam rerum.
+        <Typography sx={{ px: 10, my: 2 }}>
+          {data?.body?.description}
         </Typography>
+        <Grid container rowGap={3}>
+          <Grid size={{ lg: 3, md: 6, xs: 12 }}>
+            <Stack rowGap={0.5}>
+              <Typography fontWeight={600} fontSize="14px">
+                Contact Number
+              </Typography>
+              <Chip
+                label={
+                  <Stack direction="row" alignItems={"center"}>
+                    {data?.body?.number_verify ? (
+                      <VerifiedIcon
+                        sx={{
+                          color: "primary.main",
+                          width: "18px",
+                          height: "18px",
+                          mr: 1,
+                        }}
+                      />
+                    ) : null}
+                    <Typography fontWeight={400} fontSize="14px">
+                      {data?.body?.contact_num}
+                    </Typography>
+                  </Stack>
+                }
+                sx={{ borderRadius: 1, width: "fit-content" }}
+              />
+            </Stack>
+          </Grid>
+          <Grid size={{ lg: 3, md: 6, xs: 12 }}>
+            <Stack rowGap={0.5}>
+              <Typography fontWeight={600} fontSize="14px">
+                Business Type
+              </Typography>
+              <Chip
+                label={
+                  <Typography fontWeight={400} fontSize="14px">
+                    {data?.body?.contact_num}
+                  </Typography>
+                }
+                sx={{ borderRadius: 1, width: "fit-content" }}
+              />
+            </Stack>
+          </Grid>
+          <Grid size={{ lg: 3, md: 6, xs: 12 }}>
+            <Stack rowGap={0.5}>
+              <Typography fontWeight={600} fontSize="14px">
+                Address
+              </Typography>
+              <Chip
+                label={
+                  <Typography fontWeight={400} fontSize="14px">
+                    {data?.body?.contact_num}
+                  </Typography>
+                }
+                sx={{ borderRadius: 1, width: "fit-content" }}
+              />
+            </Stack>
+          </Grid>
+          <Grid size={{ lg: 3, md: 6, xs: 12 }}>
+            <Stack rowGap={0.5}>
+              <Typography fontWeight={600} fontSize="14px">
+                Email
+              </Typography>
+              <Chip
+                label={
+                  <Stack direction="row" alignItems={"center"}>
+                    {data?.body?.email_verify ? (
+                      <VerifiedIcon
+                        sx={{
+                          color: "primary.main",
+                          width: "18px",
+                          height: "18px",
+                          mr: 1,
+                        }}
+                      />
+                    ) : null}
+                    <Typography fontWeight={400} fontSize="14px">
+                      {data?.body?.email}
+                    </Typography>
+                  </Stack>
+                }
+                sx={{ borderRadius: 1, width: "fit-content" }}
+              />
+            </Stack>
+          </Grid>
+          <Grid size={12}>
+            <Typography variant="h5">KYC Details</Typography>
+          </Grid>
+          {data?.body?.ntn ? (
+            <Grid size={{ lg: 3, md: 6, xs: 12 }}>
+              <Stack rowGap={0.5}>
+                <Typography fontWeight={600} fontSize="14px">
+                  Company NTN Number
+                </Typography>
+                <Chip
+                  label={
+                    <Typography fontWeight={400} fontSize="14px">
+                      {data?.body?.ntn}
+                    </Typography>
+                  }
+                  sx={{ borderRadius: 1, width: "fit-content" }}
+                />
+              </Stack>
+            </Grid>
+          ) : null}
+          {data?.body?.business_name ? (
+            <Grid size={{ lg: 3, md: 6, xs: 12 }}>
+              <Stack rowGap={0.5}>
+                <Typography fontWeight={600} fontSize="14px">
+                  Company Registered Name
+                </Typography>
+                <Chip
+                  label={
+                    <Typography fontWeight={400} fontSize="14px">
+                      {data?.body?.business_name}
+                    </Typography>
+                  }
+                  sx={{ borderRadius: 1, width: "fit-content" }}
+                />
+              </Stack>
+            </Grid>
+          ) : null}
+        </Grid>
+        <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+          <Stack rowGap={2} sx={{ width: { md: "50%", xs: "100%" } }}>
+            <Typography variant="h5" sx={{ my: 2 }}>
+              Send a Query
+            </Typography>
+            <RHFTextField name="subject" outerLabel="Subject" />
+            <RHFTextField name="reason" outerLabel="Reason" />
+
+            <RHFTextField
+              name="description"
+              outerLabel="Description"
+              multiline
+              rows={5}
+            />
+
+            <LoadingButton
+              loading={isLoading}
+              variant="contained"
+              color="primary"
+              type="submit"
+              sx={{ mt: 2 }}
+            >
+              Submit
+            </LoadingButton>
+          </Stack>
+        </FormProvider>
       </Card>
     </Stack>
   );
--- src/services/admin/kyc-requests/kyc-requests-api.ts
+++ src/services/admin/kyc-requests/kyc-requests-api.ts
@@ -1,4 +1,5 @@
 import { baseAPI } from "../../base-api";
+import { APPROVE_KYC } from "../../tags";
 
 export const kycRequestsAPI = baseAPI.injectEndpoints({
   endpoints: (builder) => ({
@@ -7,26 +8,52 @@ export const kycRequestsAPI = baseAPI.injectEndpoints({
         url: `/get_donor_kyc`,
         method: "GET",
       }),
+      providesTags: [APPROVE_KYC],
     }),
     getPartnerKycRequestsList: builder.query({
       query: () => ({
         url: `/get_partner_kyc`,
         method: "GET",
       }),
+      providesTags: [APPROVE_KYC],
     }),
     getCancelPartnerKycRequestsList: builder.query({
       query: (params) => ({
         url: `/get_cancel_partner_kyc`,
         method: "GET",
-        params
+        params,
       }),
+      providesTags: [APPROVE_KYC],
     }),
     getCancelDonorKycRequestsList: builder.query({
       query: (params) => ({
         url: `/get_cancel_donor_kyc`,
         method: "GET",
-        params
+        params,
       }),
+      providesTags: [APPROVE_KYC],
+    }),
+    getProfileDetail: builder.query({
+      query: (id) => ({
+        url: `/get_profile_detail`,
+        method: "GET",
+        params: { id },
+      }),
+    }),
+    sendQuery: builder.mutation({
+      query: (body: any) => ({
+        url: "/send_query",
+        method: "POST",
+        body,
+      }),
+    }),
+    approveKYC: builder.mutation({
+      query: (body: any) => ({
+        url: "/approve_kyc",
+        method: "POST",
+        body,
+      }),
+      invalidatesTags: [APPROVE_KYC],
     }),
   }),
 });
@@ -36,4 +63,7 @@ export const {
   useGetPartnerKycRequestsListQuery,
   useGetCancelDonorKycRequestsListQuery,
   useGetCancelPartnerKycRequestsListQuery,
+  useGetProfileDetailQuery,
+  useSendQueryMutation,
+  useApproveKYCMutation,
 } = kycRequestsAPI;
